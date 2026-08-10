2aa7798ca1e19830bb0cab2349f4e3f40af1c774
zakiafuadi925-ops <zakiafuadi925@gmail.com>
2026-08-10 17:59:53 +0700

    Fixed MathGenerator

--- Assets/_Projects/Scripts/Core/MathGenerator.cs
+++ Assets/_Projects/Scripts/Core/MathGenerator.cs
@@ -2,23 +2,33 @@ using UnityEngine;
 
 namespace MathBoxing.Core
 {
-    // Struktur data untuk membungkus paket soal
     public struct MathQuestion
     {
         public string questionText;
         public int correctAnswer;
-        public int scoreValue; // Bobot skor untuk soal ini
+        public int scoreValue; 
     }
 
     public class MathGenerator : MonoBehaviour
     {
+        // Fungsi generik biasa
         public MathQuestion GenerateRandomQuestion()
+        {
+            return GenerateQuestionInternal();
+        }
+
+        // Fungsi dengan Seed khusus agar P1 dan P2 mendapat soal yang sama persis
+        public MathQuestion GenerateSeededQuestion(int seed)
+        {
+            Random.InitState(seed);
+            return GenerateQuestionInternal();
+        }
+
+        private MathQuestion GenerateQuestionInternal()
         {
             MathQuestion newQuestion = new MathQuestion();
             
-            // Acak tipe soal: 0 = Penjumlahan, 1 = Pengurangan, 2 = Perkalian
             int operationType = Random.Range(0, 3); 
-
             int num1 = Random.Range(1, 10);
             int num2 = Random.Range(1, 10);
 
@@ -27,21 +37,20 @@ namespace MathBoxing.Core
                 case 0: // Penjumlahan
                     newQuestion.questionText = $"{num1} + {num2} = ?";
                     newQuestion.correctAnswer = num1 + num2;
-                    newQuestion.scoreValue = 2; // Skor penjumlahan = 2
+                    newQuestion.scoreValue = 2;
                     break;
 
                 case 1: // Pengurangan
-                    // Agar tidak menghasilkan angka negatif di awal, kita pastikan num1 lebih besar
                     if (num1 < num2) { int temp = num1; num1 = num2; num2 = temp; }
                     newQuestion.questionText = $"{num1} - {num2} = ?";
                     newQuestion.correctAnswer = num1 - num2;
-                    newQuestion.scoreValue = 2; // Skor pengurangan = 2
+                    newQuestion.scoreValue = 2;
                     break;
 
                 case 2: // Perkalian
                     newQuestion.questionText = $"{num1} x {num2} = ?";
                     newQuestion.correctAnswer = num1 * num2;
-                    newQuestion.scoreValue = 5; // Skor perkalian = 5
+                    newQuestion.scoreValue = 5;
                     break;
             }
 
